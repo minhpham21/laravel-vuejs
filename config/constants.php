875eaedadf8bbe422d40ac99a5e6ce9fc223c101
Pham Tuan Minh <minhpham@macromillsea.com>
2022-01-04 20:08:01 +0700

    #add middleware role for admin + client

---
+++ config/constants.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'languages' => [
+        'en' => 'en',
+        'vi' => 'vi',
+    ],
+    'role' => [
+        'admin' => 1,
+        'client_admin' => 2,
+        'client' => 3,
+    ],
+];
