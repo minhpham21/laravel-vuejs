8cf998d35b15218bfbbce7fd6dd08326cdab217c
Pham Tuan Minh <minh26102111@gmail.com>
2022-01-21 01:53:03 +0700

    #refactor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,42 +2,73 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <title>@yield('title') | {{ config('app.name', 'it-shop') }}</title>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
-
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/4.4.6/css/ionicons.min.css">
-
-    <title>@yield('title') | {{ config('app.name', 'it-shop') }}</title>
     @yield('cssloader')
 </head>
 <body class="hold-transition sidebar-mini">
     <div id="app" class="wrapper">
-            @include('layouts.navbar')
-            @include('layouts.sidebar')
-            @include('layouts.content')
-            @include('layouts.footer')
-
-
-
+        @include('layouts.navbar')
+        @include('layouts.sidebar')
+        @include('layouts.content')
+        @include('layouts.footer')
     </div>
+    <!-- jQuery 3 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- SweetAlert2 -->
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script> --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/js/standalone/selectize.min.js"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function(event) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 1500
+            });
+
+            @if(Session::has('success'))
+                Toast.fire({
+                    icon: 'success',
+                    title: '{{Session::get('success')}}',
+                });
+            @endif
+
+            @if(Session::has('error'))
+                Toast.fire({
+                    icon: 'error',
+                    title: '{{Session::get('error')}}',
+                });
+            @endif
+
+            // @if(Session::has('warning'))
+            //     Toast.fire({
+            //         icon: 'warning',
+            //         title: '{{Session::get('warning')}}',
+            //     });
+            // @endif
+
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+        });
+    </script>
     @yield('script')
 </body>
 </html>
