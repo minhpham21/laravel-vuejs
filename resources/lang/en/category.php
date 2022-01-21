1128b034769821c0d39e1c55168a7525ae0c2adc
Pham Tuan Minh <minh26102111@gmail.com>
2022-01-21 14:57:49 +0700

    #update text

--- resources/lang/en/category.php
+++ resources/lang/en/category.php
@@ -5,4 +5,9 @@ return [
         'list' => 'Category List',
         'create' => 'Create category',
     ],
+    'message' => [
+        'was_status_updated' => 'Status updated!',
+        'was_created' => 'Category was created!',
+        'try_again' => 'Please try again!',
+    ],
 ];
--- resources/lang/vi/category.php
+++ resources/lang/vi/category.php
@@ -5,4 +5,10 @@ return [
         'list' => 'Danh mục sản phẩm',
         'create' => 'Tạo danh mục mới',
     ],
+    'message' => [
+        'was_status_updated' => 'Đã cập nhật trạng thái!',
+        'was_created' => 'Đã tạo thành công danh mục sản phẩm!',
+        'try_again' => 'Vui lòng thử lại!',
+
+    ],
 ];
